73c38051fde3c0d9cb7700464b45ada202cf0361
Lock Boreth Devid <lockborethdevid19@kit.edu.kh>
2023-01-02 20:31:00 +0700

    remove css bug

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -49,124 +49,124 @@ const Auth = () => {
       }
       console.log("successfully signup", response);
     });
+    navigate("/home");
   };
   return (
-    <div className="container" id="container">
-      {/* Signup Form */}
-      <div className="form-container sign-up-container">
-        <form action="" onSubmit={HandleSignupButton}>
-          <div>
-            <p></p>
-          </div>
-          <h1>Create Account</h1>
-          <span>or use your email for registration</span>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          ></input>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          ></input>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          ></input>
-          <input
-            type="tel"
-            name="phone-number"
-            placeholder="Phone Number"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-          ></input>
-          <input
-            type="text"
-            name="store-name"
-            placeholder="Store Name"
-            value={storeName}
-            onChange={(e) => setStoreName(e.target.value)}
-            required
-          ></input>
-          <input
-            type="text"
-            name="store-location"
-            placeholder="Store Location"
-            value={storeLocation}
-            onChange={(e) => setStoreLocation(e.target.value)}
-            required
-          ></input>
-          <button id="signup-button">SignUp</button>
-        </form>
-      </div>
-      {/* Signin Form */}
-      <div className="form-container sign-in-container">
-        <form action="#" onSubmit={HandleSigninButton}>
-          <h1>Sign In</h1>
-          <span>with your account</span>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          ></input>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          ></input>
-          <a href="#">Forgot Password?</a>
-          <button>Sign In</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h1>Welcome to Restaurant POS System</h1>
-            <p>Enter your personal info to sign up account</p>
-            <p class="test">
-              <span>OR</span>
-            </p>
-            <p>Already Have account ?</p>
-            <button
-              class="ghost"
-              id="signIn"
-              onClick={() => clickSigninButton()}
-            >
-              Sign In
-            </button>
-          </div>
-          <div className="overlay-panel overlay-right">
-            <h1>Welcome Back to Restaurant POS System</h1>
-            <p>Sign in to continue access our system</p>
-            <p class="test">
-              <span>OR</span>
-            </p>
-            <p>Don't have account yet?</p>
-            <button
-              className="ghost"
-              id="signUp"
-              onClick={() => clickSignupButton()}
-            >
-              Sign Up
-            </button>
+    <div className="auth-page">
+      <div className="container-auth" id="container">
+        {/* Signup Form */}
+        <div className="form-container sign-up-container">
+          <form className="form" action="" onSubmit={HandleSignupButton}>
+            <h1>Create Account</h1>
+            <span>or use your email for registration</span>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            ></input>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            ></input>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            ></input>
+            <input
+              type="tel"
+              name="phone-number"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            ></input>
+            <input
+              type="text"
+              name="store-name"
+              placeholder="Store Name"
+              value={storeName}
+              onChange={(e) => setStoreName(e.target.value)}
+              required
+            ></input>
+            <input
+              type="text"
+              name="store-location"
+              placeholder="Store Location"
+              value={storeLocation}
+              onChange={(e) => setStoreLocation(e.target.value)}
+              required
+            ></input>
+            <button id="signup-button">SignUp</button>
+          </form>
+        </div>
+        {/* Signin Form */}
+        <div className="form-container sign-in-container">
+          <form className="form" action="#" onSubmit={HandleSigninButton}>
+            <h1>Sign In</h1>
+            <span>with your account</span>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            ></input>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            ></input>
+            <a href="#">Forgot Password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome to Restaurant POS System</h1>
+              <p>Enter your personal info to sign up account</p>
+              <p class="test">
+                <span>OR</span>
+              </p>
+              <p>Already Have account ?</p>
+              <button
+                class="ghost"
+                id="signIn"
+                onClick={() => clickSigninButton()}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Welcome Back to Restaurant POS System</h1>
+              <p>Sign in to continue access our system</p>
+              <p class="test">
+                <span>OR</span>
+              </p>
+              <p>Don't have account yet?</p>
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={() => clickSignupButton()}
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
